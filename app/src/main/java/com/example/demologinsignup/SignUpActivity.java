c14306dbaf39f6768f1886a767f9e80b630ee724
devVNduc <trongducstvn@gmail.com>
2023-12-14 12:57:54 +0700

    Singup tạo toast

--- app/src/main/java/com/example/demologinsignup/SignUpActivity.java
+++ app/src/main/java/com/example/demologinsignup/SignUpActivity.java
@@ -2,20 +2,25 @@ package com.example.demologinsignup;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
     private Button btnLogin,btnSignup,btnSignupDone;
-    private EditText editTextEmail,editTextPassword,editTextRePassword,editTextName,editTextPhone;
+    private EditText editTextUsername,editTextPassword,editTextRePassword,editTextName,editTextPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +29,7 @@ public class SignUpActivity extends AppCompatActivity {
         btnSignupDone=(Button)findViewById(R.id.btnSignupDone);
         btnLogin=(Button)findViewById(R.id.btnLogin);
         btnSignup=(Button)findViewById(R.id.btnSignup);
-        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
+        editTextUsername=(EditText)findViewById(R.id.editTextUsername);
         editTextPassword=(EditText)findViewById(R.id.editTextPassword);
         editTextRePassword=(EditText)findViewById(R.id.editTextRePassword);
         editTextName=(EditText)findViewById(R.id.editTextName);
@@ -42,15 +47,26 @@ public class SignUpActivity extends AppCompatActivity {
         btnSignupDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String SignupEmail = editTextEmail.getText().toString().trim();
-                if (isValidEmail(SignupEmail)) {
-                    // Email hợp lệ
+                String SignupUsername = editTextUsername.getText().toString().trim();
+                if (isValidUsername(SignupUsername)) {
+                    // User hợp lệ
                     // Tiếp tục xử lý các bước đăng ký khác
                 } else {
                     // Email không hợp lệ
-                    Toast.makeText(SignUpActivity.this, "Email không hợp lệ", Toast.LENGTH_LONG).show();
-                    editTextEmail.requestFocus();
-                    editTextEmail.selectAll();
+                    showToast("Username không hợp lệ",R.layout.custom_toastfail_layout,R.drawable.toastfail_background);
+                    editTextUsername.requestFocus();
+                    editTextUsername.selectAll();
+                    return;
+                }
+                String SignupPhone = editTextPhone.getText().toString().trim();
+                if (isValidPhoneNumber(SignupPhone)){
+                    // Số điện thoại hợp lệ
+                    // Tiếp tục xử lý các bước đăng ký khác
+                }
+                else{
+                    showToast("Số điện thoại không hợp lệ",R.layout.custom_toastfail_layout,R.drawable.toastfail_background);
+                    editTextPhone.requestFocus();
+                    editTextPhone.selectAll();
                     return;
                 }
                 String SignupPassword = editTextPassword.getText().toString().trim();
@@ -59,7 +75,7 @@ public class SignUpActivity extends AppCompatActivity {
                     // Tiếp tục xử lý các bước đăng ký khác
                 } else {
                     // Mật khẩu không hợp lệ
-                    Toast.makeText(SignUpActivity.this, "Password không hợp lệ", Toast.LENGTH_LONG).show();
+                    showToast("Password không hợp lệ",R.layout.custom_toastfail_layout,R.drawable.toastfail_background);
                     editTextPassword.requestFocus();
                     editTextPassword.selectAll();
                     return;
@@ -70,8 +86,8 @@ public class SignUpActivity extends AppCompatActivity {
                     // Tiếp tục xử lý các bước đăng ký khác
                 }
                 else {
-                    // Mật khẩu không hợp lệ
-                    Toast.makeText(SignUpActivity.this, "Password không khớp", Toast.LENGTH_LONG).show();
+                    // Mật khẩu không trùng khớp
+                    showToast("Mật khẩu không trùng khớp",R.layout.custom_toastfail_layout,R.drawable.toastfail_background);
                     editTextRePassword.requestFocus();
                     editTextRePassword.selectAll();
                     return;
@@ -82,23 +98,14 @@ public class SignUpActivity extends AppCompatActivity {
                     // Tiếp tục xử lý các bước đăng ký khác
                 }
                 else{
-                    Toast.makeText(SignUpActivity.this, "Họ và tên không hợp lệ", Toast.LENGTH_LONG).show();
+                    showToast("Họ tên người không hợp lệ",R.layout.custom_toastfail_layout,R.drawable.toastfail_background);
                     editTextName.requestFocus();
                     editTextName.selectAll();
                     return;
                 }
-                String SignupPhone = editTextPhone.getText().toString().trim();
-                if (isValidPhoneNumber(SignupPhone)){
-                    // Số điện thoại hợp lệ
-                    // Tiếp tục xử lý các bước đăng ký khác
-                }
-                else{
-                    Toast.makeText(SignUpActivity.this, "Số điện thoại không hợp lệ", Toast.LENGTH_LONG).show();
-                    editTextPhone.requestFocus();
-                    editTextPhone.selectAll();
-                    return;
-                }
-                String TongHop = SignupName + "\n" + SignupPhone + "\n" + SignupEmail;
+
+                String TongHop = "Name: " + SignupName + "\n" + "Phone: " + SignupPhone + "\n"+ "Username: " + SignupUsername;
+                showToast("Đăng ký thành công",R.layout.custom_toastsuccess_layout,R.drawable.toastsuccess_background);
                 AlertDialog.Builder mydialog = new AlertDialog.Builder(SignUpActivity.this);
                 mydialog.setTitle("Thông tin đăng ký");
                 mydialog.setMessage(TongHop);
@@ -118,10 +125,10 @@ public class SignUpActivity extends AppCompatActivity {
     public void onBackPressed() {
         showExitConfirmationDialog();
     }
-    public boolean isValidEmail(String email) {
-        // Biểu thức chính quy để kiểm tra định dạng email
-        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-        Pattern pattern = Pattern.compile(emailRegex);
+    public boolean isValidUsername(String email) {
+        // Biểu thức chính quy để kiểm tra định dạng username
+        String usernameRegex = "^[a-zA-Z][a-zA-Z0-9_]{2,15}$";
+        Pattern pattern = Pattern.compile(usernameRegex);
 
         // Kiểm tra khớp với biểu thức chính quy
         return pattern.matcher(email).matches();
@@ -192,6 +199,32 @@ public class SignUpActivity extends AppCompatActivity {
 
         // Kiểm tra khớp với biểu thức chính quy
         return pattern.matcher(fullName).matches();
+    }
+    public void showToast(String message,int layoutResId, int drawableResId) {
+        // Tạo Toast
+        Toast toast = new Toast(getApplicationContext());
+
+        // Thiết lập thời gian hiển thị và vị trí của Toast
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 1200);
+
+        // Tạo đối tượng LayoutInflater để inflate custom layout
+        LayoutInflater inflater = getLayoutInflater();
+            View layout = inflater.inflate(layoutResId, findViewById(R.id.custom_toast_container));
+
+        // Thiết lập layout cho Toast
+        toast.setView(layout);
+
+        // Thiết lập văn bản của Toast
+        TextView textView = layout.findViewById(R.id.toast_text);
+        textView.setText(message);
+
+        // Thiết lập hình dạng bo góc cho Toast
+        Drawable background = ContextCompat.getDrawable(getApplicationContext(), drawableResId);
+        layout.setBackground(background);
+
+        // Hiển thị Toast
+        toast.show();
     }
     private void showExitConfirmationDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
