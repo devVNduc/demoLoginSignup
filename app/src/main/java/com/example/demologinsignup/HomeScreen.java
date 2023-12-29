4773e210b8f2d1b0c18799edb9173791157a228a
binh12345jejec <binhprovip01@gmail.com>
2023-12-29 14:59:21 +0700

    đăng xuất

--- app/src/main/java/com/example/demologinsignup/HomeScreen.java
+++ app/src/main/java/com/example/demologinsignup/HomeScreen.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
@@ -41,6 +45,49 @@ public class HomeScreen extends AppCompatActivity {
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
 
+        if (itemId == R.id.logoutMenuItem) {
+            // Gọi phương thức đăng xuất khi menu "Đăng xuất" được chọn
+            logout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean checkLoginStatus() {
+        return false;
+    }
+
+    private void logout() {
+
+        // Kiểm tra trạng thái đăng nhập
+        boolean isLoggedIn = checkLoginStatus();
+
+        if (!isLoggedIn) {
+            // Nếu chưa đăng nhập, chuyển đến màn hình đăng nhập và kết thúc MainActivity
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+            return;
+        }
+
+        setContentView(R.layout.activity_main);
+        // Chuyển đến màn hình đăng nhập hoặc màn hình khác tùy thuộc vào logic của ứng dụng
+        resetAppState();
+        // Đóng màn hình hiện tại (MainActivity) để ngăn người dùng trở lại nó bằng cách nhấn nút "Back"
+        finish();
+    }
+
+    private void resetAppState() {
+    }
 }
+
